cdb3012b502655f317cdc1b493b9007779f0dada
Satish Nayak <satish_nayak@epam.com>
2023-03-15 16:02:19 -0400

    first commit

---
+++ springbatchasync/src/main/java/com/instanceofcake/springbatchasync/SpringbatchasyncApplication.java
@@ -0,0 +1,13 @@
+package com.instanceofcake.springbatchasync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchasyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbatchasyncApplication.class, args);
+	}
+
+}
---
+++ springbatchasync/src/test/java/com/instanceofcake/springbatchasync/SpringbatchasyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.instanceofcake.springbatchasync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbatchasyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
